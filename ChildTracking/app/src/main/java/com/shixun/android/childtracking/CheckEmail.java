eff6e9847afef2c7c68d000e904fd307c7d465b7
mengyuGong <30542461+mengyuGong@users.noreply.github.com>
2017-09-29 19:07:14 +1000

    call api

---
+++ ChildTracking/app/src/main/java/com/shixun/android/childtracking/CheckEmail.java
@@ -0,0 +1,13 @@
+package com.shixun.android.childtracking;
+
+/**
+ * Created by gongmengyu on 2017/9/29.
+ */
+
+public class CheckEmail {
+
+    public String email;
+    public String password;
+    public String type;
+    public String result;
+}
